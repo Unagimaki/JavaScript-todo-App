2f3c6480ef947f956968ff3a9cf4c97783a6865b
Unagimaki <m.yark0v.2000@gmail.com>
2023-06-27 03:29:07 +0400

    error correction

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,26 @@
 const addBtn = document.querySelector('.btn_add')
-const body = document.querySelector('.todo_body')
+let body = document.querySelector('.todo_body')
 addBtn.addEventListener('click', addTodo)
 let todos = []
 
-html = ''
-body = html
 
 
 function addTodo() {
     let add = prompt('Название дела')
     todos.push(add)
-
-    let li = document.createElement('li')
-    li.innerHTML = add
-    body.append(li)
-
+    render()
 }
 
 function render() {
+    let html = ' '
     todos.forEach((item) => {
-        html += 
+        console.log(item)
+        html +=
         `
-            <li class="todo_item">${add}</li>
+            <li class="todo_item">${item}</li>
         `
     })
+    console.log(html)
+    body.innerHTML = html 
 }
+
