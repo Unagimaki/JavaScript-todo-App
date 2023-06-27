638c14dcd1a1aafe315783fe741a83f1c6e38377
Unagimaki <m.yark0v.2000@gmail.com>
2023-06-27 12:38:57 +0400

    Added new features
    Delete function and mark as important function

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,72 @@
 const addBtn = document.querySelector('.btn_add')
 const body = document.querySelector('.todo_body')
+
 addBtn.addEventListener('click', addTodo)
+
 let todos = []
+console.log(todos.length)
+let text = 'Пустой список'
 
 
+
+let todo = {
+    name: ' ',
+    marked: false,
+    id: Math.random().toFixed(3)
+}
 
 function addTodo() {
     let add = prompt('Название дела')
-    todos.push(add)
+    todos.push(
+        {
+            name: add,
+            marked: false,
+            id: Math.random().toFixed(3)
+        }
+    )
     render()
 }
 
 function render() {
     let html = ' '
-    todos.forEach((item) => {
+    todos.map((item, index) => {
+        item.marked ? html +=
+        `
+        <div class="todo_item marked">
+            <li >${item.name}</li><button onclick="removeTodo(${item.id})" class="delete_btn">Удалить</button><button onclick="markTodo(${item.id})" class="mark_btn">Важное</button>
+        </div>
+        `
+        :
         html +=
         `
-            <li class="todo_item">${item}</li>
+        <div class="todo_item">
+            <li >${item.name}</li><button onclick="removeTodo(${item.id})" class="delete_btn">Удалить</button><button onclick="markTodo(${item.id})" class="mark_btn">Важное</button>
+        </div>
         `
+
     })
-    body.innerHTML = html 
+    body.innerHTML = html;
+    todos.length === 0 ? body.innerHTML = text : false
+
 }
 
+
+function removeTodo(id) {
+    todos.forEach((item, index) => {
+        item.id == id ? todos.splice(index, 1)
+        : false
+    })
+
+    render()
+}
+
+function markTodo(id) {
+    todos.map((item) => {
+        item.id == id && item.marked ? item.marked = false :
+        item.id == id && !item.marked ? item.marked = true :
+        false
+    })
+    render(id)
+}
+
+render()
